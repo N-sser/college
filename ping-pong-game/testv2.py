9f9bf0a3d48fda6cf35de219c1be062d57ee9913
N-sser <196800683+N-sser@users.noreply.github.com>
2025-09-08 22:48:02 -0500

    "Add configuration parameters and ping pong classes.

---
+++ ping-pong-game/testv2.py
@@ -0,0 +1,108 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('PingPongUltimate')
+root.resizable(False, False)   
+window_width = 1600
+window_height = 1200
+
+# Get the screen dimensions to find the "center" point of the window
+center_x = int(root.winfo_screenwidth() / 2 - window_width / 2)
+center_y = int(root.winfo_screenheight() / 2 - window_height / 2)
+root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
+
+game_canvas = tk.Canvas(root,
+    width = window_width, 
+    height = window_height, 
+    bg = "#1d2021",
+    highlightthickness = 0
+    )
+game_canvas.pack()
+
+class GameObject:
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.canvas_id = None
+    
+    def render(self, canvas):
+        pass
+
+    def update_position(self, canvas):
+        pass
+
+class Ball(GameObject):
+    def __init__(self, x, y, radius, color, speed_x = 0, speed_y = 0):
+        super().__init__(x, y, radius*2, radius*2, color)
+        
+        self.radius= radius
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+        
+    def render(self, canvas):
+        self.canvas_id = canvas.create_oval(
+            self.x, self.y,
+            self.x + self.width, self.y + self.height,
+            fill = self.color)
+    
+    def bounce_vertical(self):
+        self.speed_y *= -1
+    
+    def bounce_horizontal(self, speed_increase = 1.05):
+        self.speed_x *= -speed_increase
+
+    def update_position(self, canvas):
+        self.x += self.speed_x
+        self.y += self.speed_y
+        canvas.coords(self.canvas_id, 
+            self.x, self.y,
+            self.x + self.radius * 2, self.y + self.radius * 2)
+
+class Racket(GameObject):
+    def __init__(self, x, y, width, height, color, speed = 15):
+        super().__init__(x, y, width, height, color)
+        
+        self.speed_y: float = speed
+        
+    def render(self, canvas):
+        self.canvas_id = canvas.create_rectangle(
+            self.x, self.y,
+            self.x + self.width, self.y + self.height,
+            fill = self.color)
+    
+    def update_position(self, canvas):
+        self.y += self.speed_y
+        canvas.coords(self.canvas_id,
+            self.x, self.y,
+            self.x + self.width, self.y + self.height)
+
+class Net(GameObject):
+    def __init__(self, x, y, width, height, color):
+        super().__init__(x, y, width, height, color)
+
+    def render(self, canvas):
+        self.canvas_id = canvas.create_line(
+            self.x, self.y, 
+            self.x, self.height, 
+            fill = self.color,
+            #dash = )
+        )
+    
+    def update_position(self, canvas):
+        return super().update_position(canvas)
+
+ball = Ball(100, 100, 25, 'pink')
+ball.render(game_canvas)
+ball.update_position(game_canvas)
+
+left_racket = Racket(400, 400 , 20 ,150, 'orange', 10)
+left_racket.render(game_canvas)
+left_racket.update_position(game_canvas)
+
+net = Net(window_width / 2, 0, 100, window_height, 'white')
+net.render(game_canvas)
+net.update_position(game_canvas)
+root.mainloop()
